fd2dea4119d6814c3f40290aa403dc7bf890df3b
Hotmilx <spotam005@gmail.com>
2025-06-15 16:37:50 +0400

    add

---
+++ script.js
@@ -0,0 +1,123 @@
+'use strict'
+
+document.getElementById('form1').addEventListener('submit', function (e){
+    e.preventDefault();
+    let name = this.elements['name'].value.trim();
+    let lastName = this.elements['lastName'].value.trim();
+    let message = this.elements['msg'].value.trim();
+    let email = this.elements['email'].value.trim();
+
+    if (name.length < 3) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Имя должно содержать не менее 3 символов!'
+        })
+    }
+    else if (lastName.length < 3) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Фамилия должна содержать не менее 3 символов!',
+        })
+    }
+    else if (email == '') {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Поле почты не может быть пустым!'
+
+        })
+    }
+    else if (message == '') {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Сообщение не может быть пустым!',
+            })
+    }
+    else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Сообщение отправлено, ожидайте ответа'
+
+        })
+    }
+
+    const request = new XMLHttpRequest();
+    request.onload = (event) => console.log("request finished");
+    request.open("GET", "http://127.0.0.1");
+    request.send();
+
+})
+
+document.getElementById('form2').addEventListener('submit', function (e){
+    e.preventDefault();
+
+    let name = this.elements['fName'].value.trim();
+    let address = this.elements['address'].value.trim();
+    let comment = this.elements['comment'].value.trim();
+    let check = this.elements['checkbox'].checked;
+
+    if (name.length < 3) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Имя должно содержать не менее 3 символов!'
+        })
+    }
+    else if (address.length < 5) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Адрес должен содержать не менее 5 символов!'
+        })
+    }
+    else if (!check) {
+            Swal.fire({
+            icon: 'warning',
+            title: 'Ошибка',
+            text: 'Необходимо принять условия договора-оферты'
+        })
+    }
+
+    else { 
+    Swal.fire({
+        title: 'Заказ создан',
+        html: `
+        <p><strong>Имя:</strong> ${name}</p>
+        <p><strong>Адрес:</strong> ${address}</p>
+        <p><strong>Комментарий:</strong> ${comment || 'Отсутствует'}</p>
+        `,
+        icon: 'success',
+        });
+    }
+
+    const request = new XMLHttpRequest();
+    request.onload = (event) => console.log("request finished");
+    request.open("GET", "http://127.0.0.1");
+    request.send();
+
+})
+
+
+fetch('reviews.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('responsesRow');
+
+        data.forEach(review => {
+            const rev = document.createElement('div');
+            rev.className = 'response';
+
+            rev.innerHTML = `
+            <div class="responseHead">
+                <img src="1110.png" alt="">
+                <p class="responseTitle">${review.title}</p>
+            </div>
+            <p class="responseText">${review.text}</p>
+            `;
+
+            container.appendChild(rev);
+        });
+    });
